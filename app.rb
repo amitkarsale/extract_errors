e732df3ef53338883bf1098768c8760bc11a4473
Amit Karsale <karsale.amit@gmail.com>
2017-02-13 20:56:00 +0530

    basic layout, filtering and ordering of data

--- app.rb
+++ app.rb
@@ -6,19 +6,15 @@ require './models/error'
 require './lib/parse_html'
 
 get '/' do
-	@errors = Error.all
-    erb :index
+	if params[:filter] && params[:filter].downcase == "uniq"
+		@errors = Error.select("message").group(:message).order('count_message DESC').count
+		erb :error
+	else
+		@errors = Error.all
+    	erb :index
+	end
 end
 
 get '/feed_data' do
     ParseHtml.new
-end
-
-post '/submit' do
-	@model = Error.new(params[:error])
-	if @model.save
-		redirect '/errors'
-	else
-		"Sorry, there was an error!"
-	end
 end
