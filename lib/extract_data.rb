66312afe5a5fa3c9209c54a8c0da01907fa28fe2
Amit Karsale <karsale.amit@gmail.com>
2017-03-06 18:44:09 +0530

    revised for error handling

--- lib/extract_data.rb
+++ lib/extract_data.rb
@@ -15,7 +15,8 @@ class ExtractData
 		@task_cases.each do |kase|
 			kase_number = "0" + kase.casenumber
 			case_split = kase_number.scan(/.{3}/)
-			Dir.mkdir("/home/akarsale/tmp/task-export")
+			FileUtils::mkdir_p '/home/akarsale/tmp/task-export'
+			# Dir.mkdir("/home/akarsale/tmp/task-export")
 			if case_split[1].to_i  <= 600
 				system("cp ~/cases/#{case_split[0]}/#{case_split[1]}/#{case_split[2]}/attachments/task-export* ~/tmp/task-export;
 					for f in ~/tmp/task-export/*; do tar xf $f; done; ")
@@ -36,10 +37,10 @@ class ExtractData
 			Dir.chdir(curr_dir)
 			files = Dir.glob pwd + "/task-export*"
 			files.each do |file|
-				path  = pwd + "/" + file
-				system("for f in #{path}; do tar xf $f; done;")
-				dir_path = Dir.glob pwd + "tmp/*"
-				extract_path = pwd +"/"+ dir_path[0]
+				system("for f in #{file}; do tar xf $f --directory #{pwd}; done;")
+				dir_path = Dir.glob pwd + "/tmp/*"
+				extract_path = dir_path[0]
+				binding.pry
 				ParseHtml.new(extract_path, kase_number)
 				FileUtils.rm_rf("#{extract_path}")
 				# system("rm -rf #{extract_path}; rm -rf #{file}")
